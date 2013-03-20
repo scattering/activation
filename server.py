32eca31239ed1b656240597ed1f87da30a26ceb5
Paul Kienzle <paul.kienzle@nist.gov>
2013-03-20 17:43:22 -0400

    use threaded server so IE doesn't hanga

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
  
 import os
-import BaseHTTPServer
+from BaseHTTPServer import HTTPServer
+from SocketServer import ThreadingMixIn
 import CGIHTTPServer
 import cgitb; cgitb.enable()  ## This line enables CGI error reporting
  
-server = BaseHTTPServer.HTTPServer
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    allow_reuse_address = True
+    request_queue_size = 50
+
+server = ThreadedHTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
 server_address = ("", 8000)
 handler.cgi_directories = ["/cgi-bin"]
